c87905e6d9a232ed681c9c738567c27198d613d1
Juan Esteban Pelaez <juanes413@outlook.com>
2025-01-06 10:56:17 -0500

    Se creo view del splash con animaciones

--- WeatherApp/Domain/Splash/SplashView.swift
+++ WeatherApp/Domain/Splash/SplashView.swift
@@ -10,12 +10,62 @@ import SwiftData
 
 struct SplashView: View {
     
+    let duration: Int
+    
+    @State private var isAnimating = false
+    
     var body: some View {
-        Text("Splash View")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color(red: 18/255, green: 30/255, blue: 108/255),
+                                                       Color(red: 238/255, green: 66/255, blue: 78/255)]),
+                           startPoint: .leading, endPoint: .trailing)
+            .ignoresSafeArea()
+            
+            VStack(spacing: 40) {
+                Text(Constants.Messages.titleSplash)
+                    .font(.largeTitle)
+                    .bold()
+                Image(systemName: Constants.Images.sun)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .rotationEffect(.degrees(self.isAnimating ? 360 : 0))
+                    .animation(Animation.linear(duration: TimeInterval(self.duration/2)).repeatForever(autoreverses: false), value: self.isAnimating)
+            }
+            HStack(spacing: 12) {
+                ForEach(0..<self.duration, id: \.self) { index in
+                    DropView(duration: index, isAnimating: self.isAnimating)
+                }
+            }
+        }
+        .foregroundColor(.white)
+        .onAppear {
+            self.isAnimating = true
+        }
+        .onDisappear {
+            self.isAnimating = false
+        }
+    }
+    
+}
+
+struct DropView: View {
+    
+    let duration: Int
+    let isAnimating: Bool
+    
+    var body: some View {
+        let size = UIScreen.main.bounds.height/2
+        Image(systemName: Constants.Images.drop)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 24, height: 24)
+            .offset(y: self.isAnimating ? size-(size/3) : -size)
+            .animation(.easeInOut(duration: TimeInterval(self.duration)), value: self.isAnimating)
     }
     
 }
 
 #Preview {
-    return SplashView()
+    return SplashView(duration: 4)
 }
